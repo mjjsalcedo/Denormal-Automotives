3f64e8274be356ebe544ac6fabe54df4e88d2043
Mariel Salcedo <mjjsalcedo@yahoo.com>
2017-07-25 09:47:56 -1000

    denormal 5 done

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -6,5 +6,8 @@ DROP USER IF EXISTS denormal_user;
 CREATE USER denormal_user;
 CREATE DATABASE denormal_cars WITH OWNER denormal_user;
 
-\c denormal_user
+\c denormal_cars
 \i scripts/denormal_data.sql
+
+SELECT DISTINCT make_title
+FROM car_models;
